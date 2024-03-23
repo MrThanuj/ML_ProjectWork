63d4b270a06697808cba6ca6ddf3cb0de603edeb
Thanuj Kumar Shivalingaiah <txs6007@mavs.uta.edu>
2024-03-23 01:40:55 -0500

    exception & logging

--- src/logger.py
+++ src/logger.py
@@ -2,14 +2,30 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(logs_path,exist_ok=True)
+# Create a log file name with the current timestamp
+log_filename = f"log_{datetime.now().strftime('%Y%m%d_%H%M%S')}.log"
 
-LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+# Define the directory where logs will be stored
+log_directory = os.path.join(os.path.dirname(__file__), "application_logs")
 
+# Ensure the directory exists; if not, create it
+os.makedirs(log_directory, exist_ok=True)
+
+# Construct the full path for the log file
+full_log_path = os.path.join(log_directory, log_filename)
+
+# Configure the logging
 logging.basicConfig(
-    filename=LOG_FILE_PATH,
-    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO,
+    filename=full_log_path,
+    filemode='a',  # Append mode
+    format='[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    level=logging.INFO
 )
+
+# Expose a custom log function for external use
+def custom_log(message, level=logging.INFO):
+    if level == logging.ERROR:
+        logging.error(message)
+    else:
+        logging.info(message)
